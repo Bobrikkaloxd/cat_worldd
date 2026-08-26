63093cbb78096a21fb3d8a2abab3f11d79246500
Bobrikkaloxd <grisaaa720@gmail.com>
2026-08-26 11:20:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const cats = [ { name: "Барсик", emoji: "🐱", description: "Любит спать на клавиатуре и мешать хозяину работать." }, { name: "Гордюша", emoji: "😺", description: "Профессиональный специалист по выпрашиванию вкусняшек." }, { name: "Симба", emoji: "🐈", description: "Храбрый кот, который уверен, что весь дом принадлежит ему." }, { name: "Луна", emoji: "🌙", description: "Ночная охотница за шорохами и невидимыми призраками." } ];
-const button = document.querySelector("button");
-button.addEventListener("click", function () { const randomCat = cats[Math.floor(Math.random() * cats.length)];
-alert(
-    `${randomCat.emoji} ${randomCat.name}\n\n${randomCat.description}`
-);});
+const cats = [
+    "🐱 Сегодня твой котик — Гордюша! 🩶",
+    "🧡 Сегодня твой котик — Симба!",
+    "🖤 Сегодня твой котик — Филя!"
+];
+
+const button = document.getElementById("randomCat");
+const result = document.getElementById("result");
+
+button.addEventListener("click", function () {
+
+    const randomIndex = Math.floor(Math.random() * cats.length);
+
+    result.textContent = cats[randomIndex];
+
+});
